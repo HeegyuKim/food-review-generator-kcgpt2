a7a91e071aeb8ea6e57d5f4c0eedcd1c1d189dbf
EC2 Default User <ec2-user@ip-172-31-39-9.ap-northeast-2.compute.internal>
2022-02-12 04:32:21 +0000

    기본 카운트 1개로 하고 spinner 넣음

--- app_st.py
+++ app_st.py
@@ -18,18 +18,19 @@ positive = st.radio(
 positive = positive == select_types[0]
 
 prefix = st.text_input("리뷰의 시작 텍스트를 입력해주세요", "이 가게 음식은")
+count = st.slider('몇 개 생성하실 건가요?', 1, 10, 1)
 
-def generate_review(reviewer, prefix, positive):
+def generate_review(reviewer, prefix, positive, count):
     text = "긍정 : " if positive else "부정 : "
     text += prefix
-    return reviewer(text, num_return_sequences=5)
+    return reviewer(text, max_length=100, num_return_sequences=count)
     
 with st.spinner("리뷰를 작성중이에요..."):
     reviewer = get_pipeline()
 
 refresh = st.button("새로고침")
 
-for text in generate_review(reviewer, prefix, positive):
+for text in generate_review(reviewer, prefix, positive, count):
     text = text["generated_text"]
     text = text.split("\n")[0]
     
